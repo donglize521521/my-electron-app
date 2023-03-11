ba9acc9f38f1a788a3c3e4be7ac6e099e2dda0e7
星霜下逆天生 <1486252015@qq.com>
2023-03-11 09:15:24 +0800

    feat(06): 窗口尺寸大小设计

--- main.js
+++ main.js
@@ -5,9 +5,14 @@ const createWindow = () => {
   const win = new BrowserWindow({
     x:100, //调整初始化窗口默认x展示位置
     y:100, //调整初始化窗口默认y展示位置
-    show: false,
-    width: 800,
-    height: 600,
+    show: false, //默认情况下一个窗口对象之后就会显示，设置为false就不会显示了
+    width: 800, 
+    height: 400,
+    maxHeight: 600, //最大高度
+    maxWidth: 1000, //最大宽度
+    minHeight:200,  //最小高度
+    minWidth: 300,  //最小宽度
+    resizable: false, //是否允许缩放应用的窗口大小
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
     },
