e2257f052354de2f3e18c8ca53ebe51ce317c1ab
星霜下逆天生 <1486252015@qq.com>
2023-03-11 09:02:44 +0800

    feat(06): 解决初始化白屏的问题

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const path = require("path");
 
 const createWindow = () => {
   const win = new BrowserWindow({
+    x:100, //调整初始化窗口默认x展示位置
+    y:100, //调整初始化窗口默认y展示位置
+    show: false,
     width: 800,
     height: 600,
     webPreferences: {
@@ -11,7 +14,9 @@ const createWindow = () => {
   });
 
   win.loadFile("index.html");
-
+  win.on('ready-to-show',()=>{
+    win.show()
+  })
   win.on('close', ()=> {
     console.log('mainWin is closed')
     mainWin = null
